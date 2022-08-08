7d693711ea2d2c3b73d271db52d2a6193895a216
Dibyajyoti Mishra <dibyajyotimishra14@gmail.com>
2022-08-08 18:56:11 +0530

    chore: update npm check version rule

--- internals/npmcheckversion.js
+++ internals/npmcheckversion.js
@@ -4,7 +4,7 @@ exec("npm -v", (error, version) => {
   if (error) {
     throw error;
   }
-  if (parseFloat(version) < 6 || parseFloat(version) > 7) {
+  if (parseFloat(version) < 6) {
     throw new Error(
       `[ERROR]: You are using a wrong version of npm. Please update/downgrade to version 6.14.15.\n Your CURRENT VERSION: ${version}`
     );
