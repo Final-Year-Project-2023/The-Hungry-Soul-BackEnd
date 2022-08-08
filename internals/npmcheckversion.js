bf9441961f49584da2e96f659d625e117af9dc01
Dibyajyoti Mishra <dibyajyotimishra14@gmail.com>
2022-08-08 16:03:08 +0530

    chore: add node version checker to maintain uniformity

---
+++ internals/npmcheckversion.js
@@ -0,0 +1,12 @@
+const { exec } = require("child_process");
+
+exec("npm -v", (error, version) => {
+  if (error) {
+    throw error;
+  }
+  if (parseFloat(version) < 6 || parseFloat(version) > 7) {
+    throw new Error(
+      `[ERROR]: You are using a wrong version of npm. Please update/downgrade to version 6.14.15.\n Your CURRENT VERSION: ${version}`
+    );
+  }
+});
